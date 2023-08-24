350834f1b08538a343ff60ab74662ba3d281ec01
felipepmoreno <felipepmoreno@gmail.com>
2023-08-24 18:14:37 -0300

    Ajustes na box

--- src/App.js
+++ src/App.js
@@ -11,17 +11,30 @@ function App() {
         <h1>Felipe Pereira Moreno</h1>
       </div>
       <div className="content">
-      <h2>Contatos</h2>
+
+        <h2>Contatos</h2>
         <ul>
-          <li>Email: seuemail@email.com</li>
-          <li>Telefone: (xx) xxxx-xxxx</li>
-          <li>LinkedIn: link_para_seu_perfil</li>
-          <li>GitHub: link_para_seu_perfil</li>
+          <li>Email: felipepmoreno@gmail.com</li>
+          <li>Telefone: +55 (11) 98383-2807</li>
+          <li>LinkedIn: https://www.linkedin.com/in/felipe-pereira-moreno-b11207196/</li>
+          <li>GitHub: https://github.com/felipepmoreno</li>
         </ul>
-        <h2>Resumo</h2>
+
+        <h2>Resumo Profissional</h2>
         <p>
-          Escreva aqui um breve resumo sobre você, sua formação, experiência e habilidades.
-        </p>       
+        Engenheiro de Software com Bacharelado em Ciência da Computação pelo Centro Universitário FEI e formação
+        técnica em Engenharia Mecatrônica pelo SENAI.
+        Nos últimos 2 anos, utilizou o framework .NET Core (C#) para desenvolver sistemas robustos e Web APIs,
+        incluindo sistemas que gerenciavam milhares de mensagens por dia de middlewares. Possui sólido
+        entendimento sobre bancos de dados (SQL, MongoDB), serviços de mensagens (Azure Service Bus, Redis,
+        RabbitMQ) e ambientes em nuvem (AWS, Azure/Azure DevOps). Experiente no uso do Git, na implementação
+        de arquiteturas de software (microserviços, MVC) e com práticas de desenvolvimento (DDD, TDD) em
+        ambientes Agile/SCRUM.
+        Colaborativo e comprometido em manter fortes relações com colegas, também busca crescimento profissional,
+        como demonstrado pela obtenção da certificação Microsoft AZ900. Em busca ativa de um papel desafiador para
+        aplicar e expandir ainda mais as habilidades de engenharia de software.
+        </p>
+
       </div>
     </div>
   );
